615ab5f009201da067c9c20222b1e97486d96049
cleison0silva <78964998+cleison0silva@users.noreply.github.com>
2022-09-21 07:19:03 -0300

    Update api_dolar.py

--- api_dolar.py
+++ api_dolar.py
@@ -1,9 +1,13 @@
 import requests
 
 req = requests.get("https://economia.awesomeapi.com.br/all/USD-BRL")
-cotacao = req.json()
+info = req.json()
+
+moeda = info["USD"]["name"]
+data = info["USD"]["create_date"]
+cotacao = float(info["USD"]["bid"])
 
 print("###Cotação do Dólar###\n")
-print("Moeda: " + cotacao["USD"]["name"])
-print("Data: " + cotacao["USD"]["create_date"])
-print("Valor Atual: " + cotacao["USD"]["bid"])
+print(f"Moeda: {moeda}")
+print(f"Data: {data}")
+print(f"Valor Atual: {cotacao:.2f}")
